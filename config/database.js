6512a28607f2e4d7c69201e50b0fbaf05e31b179
Rizka <rizka.auliarahmi.tif419@polban.ac.id>
2022-06-09 21:04:10 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -9,6 +9,9 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'WebsiteJTK-3B'),
         uri: env('DATABASE_URI', 'mongodb://Kelompok3B:Kelompok3B@webjtkb3-shard-00-00.ldupa.mongodb.net:27017,webjtkb3-shard-00-01.ldupa.mongodb.net:27017,webjtkb3-shard-00-02.ldupa.mongodb.net:27017/?ssl=true&replicaSet=atlas-c328xs-shard-0&authSource=admin&retryWrites=true&w=majority'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },    
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
